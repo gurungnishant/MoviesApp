ada4b93522ab1bb44dbdc172981a94837fdb1315
Venkata Sai Suman Bhusarapu <suman@unknown062192a4f5ab.attlocal.net>
2021-09-12 16:54:21 -0500

    simple login page with routing

---
+++ src/app/movies/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsersService } from 'src/app/services/users.service';
+import { Router } from '@angular/router'
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  input_username:string 
+  input_pwd:string
+  user_id: any
+  error_cond: boolean = false
+  status: boolean = false
+  
+  constructor( private usersService: UsersService,private router: Router) { 
+    this.input_username = ''
+    this.input_pwd = ''
+  }
+
+  ngOnInit(): void {
+  }
+  onClickSubmit(formobj : any){
+    //get form details into local variables (user entered variables)
+    this.input_username = formobj.user_name
+    this.input_pwd = formobj.user_pwd
+    //pass user entered information to service to validate
+    let serviceobj = this.usersService.getUsers(this.input_username,this.input_pwd)
+    //unpack the response into local variables
+    this.user_id = serviceobj[1]
+    this.status = serviceobj[0]
+    //set the response user id information and status of rendering
+    if (this.status) {
+      this.user_id = serviceobj[1]
+      this.error_cond = false
+      this.router.navigate(['/home',this.user_id])//pass the user id to the next component (that is home component)
+    }else{
+      this.error_cond = true
+    }
+  }
+
+}
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+
+let checkstatus : any [] = []
+let userdata : any [] = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "password": "1234",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Micheal Scott",
+    "password": "1234",
+    "username": "Prison Mike",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
+]
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  getUsers(input_username : string, input_pwd : string){
+    checkstatus[0] = false
+    if (input_username != '' && input_pwd != '') {
+      for(let i=0; i<userdata.length; i++){
+        if (userdata[i].username == input_username && userdata[i].password == input_pwd) {
+          checkstatus[0] = true
+          checkstatus[1] = userdata[i].id
+        }
+      }
+      if (checkstatus[0] == false) {
+          checkstatus[0] = false
+          checkstatus[1] = 404
+      }
+    }
+    return checkstatus;
+  }
+  constructor() { }
+}
