6b8efd815bb7b7a8e8efa15d26336b05b4a83086
nishantgurungatgit <nishantformal@gmail.com>
2021-09-16 19:46:37 -0500

    behavior subject rxjs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,18 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   userId: any;
+  userLogStatus: any;
   constructor(
     private route: ActivatedRoute,
     private userService: UsersService,
-    private router: Router
+    private router: Router,
+    private userLoggedInData: UsersService
   ) {}
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.params.id;
+    this.userLoggedInData.currentStatus.subscribe(userLogStatus => this.userLogStatus = userLogStatus)
   }
 
   logOutUser() {
-    this.userService.logoutUser();
+    //this.userService.logoutUser();
+    this.userLoggedInData.changeMessage(false);
+    
     this.router.navigate(['/login']);
   }
 }
--- src/app/movies/dashboard/dashboard.component.ts
+++ src/app/movies/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '../movie';
 
+
 // let moviedata =  {
 //   "id": "tt0111161",
 //   "rank": "1",
@@ -23,15 +24,22 @@ import { Movie } from '../movie';
 })
 export class DashboardComponent implements OnInit {
   movies: Movie[] = [];
+  userLogStatus: any;
 
   constructor(
     private movieObj: MoviesService,
     private userService: UsersService,
-    private router: Router
+    private router: Router,
+    private userLoggedInData: UsersService
   ) {}
 
+
   ngOnInit(): void {
-    if (this.userService.getLoggedInStatus()) {
+
+    this.userLoggedInData.currentStatus.subscribe(userLogStatus => this.userLogStatus = userLogStatus)
+
+
+    if (this.userLogStatus) {
       this.movies = this.movieObj.getAllMovies();
     } else {
       this.router.navigate(['/login']);
